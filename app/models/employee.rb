44cf265dd554e44d1ad105f0bf6a43b2aab996f5
Steven Vo <findme@stevenvo.info>
2015-04-28 22:20:08 +0000

    added employee model

---
+++ app/models/employee.rb
@@ -0,0 +1,4 @@
+class Employee < ActiveRecord::Base
+  has_many :invoices
+  
+end
---
+++ db/migrate/20150428071132_remove_sales_person_from_invoices.rb
@@ -0,0 +1,6 @@
+class RemoveSalesPersonFromInvoices < ActiveRecord::Migration
+  def change
+    remove_column :invoices, :salesperson, :string
+    add_column :invoices, :salesperson_id, :integer
+  end
+end
